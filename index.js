8bc5430efbcb6e48151494402225710b18ef8200
thomas mariotte <thomasmariotte@hotmail.fr>
2023-11-29 11:44:30 +0100

    Better graph

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ const shouldMigrate = !fs.existsSync(filename)
  *
  */
 function * generateContacts () {
- // TODO
-
 // let's create a loop
   for (let i = 0; i <= numContacts; i++) {
     yield [`name-${i}`, `email-${i}@domain.tld`]
@@ -48,54 +46,52 @@ const insertContacts = async (db) => {
         query = query + `('${name}', '${email}'), `
         count += 1
 
-        if (count === 800) {
+        if (count === 1000) {
             await db.run(query.slice(0, -2))
             query = 'INSERT INTO contacts (name, email) VALUES '
             count = 0
         }
-
-        console.log(`Correctly inserted contact ${name}`)
     }
     
     if (count > 0) {
         await db.run(query.slice(0, -2))
     }
-}
 
 
-// query
+
 const queryContact = async (db) => {
-  const start = Date.now();
-  const res = await db.get("SELECT name FROM contacts WHERE email = ?", [
-    `email-${numContacts}@domain.tld`,
-  ]);
-  if (!res || !res.name) {
-    console.error("Contact not found");
-    process.exit(1);
-  }
-  const end = Date.now();
-  const elapsed = end - start;
-  console.log(`Query took ${elapsed} milliseconds`);
-  
-  // Log the results to a document
-  const logEntry = `${numContacts} contacts, ${elapsed} milliseconds\n`;
-    fs.appendFileSync("query_InsertTime.txt", logEntry);
-    
-  // Making a graph from the table
-  const graphEntry = `size : ${numContacts}, mill :${elapsed}\n`;
-  fs.appendFileSync("graph_InsertTime.csv", graphEntry);
+    const start = Date.now();
+    const res = await db.get("SELECT name FROM contacts WHERE email = ?", [`email-${numContacts}@domain.tld`,]);
 
+    console.log(res);
+
+    if (!res || !res.name) {
+        console.error("Contact not found");
+        process.exit(1);
+    }
+    const end = Date.now();
+    const elapsed = end - start;
+    console.log(`Query took ${elapsed} milliseconds`);
+    
+    // Log the results to a document
+    const logEntry = `${numContacts} contacts, ${elapsed} milliseconds\n`;
+        fs.appendFileSync("query_InsertTime.txt", logEntry);
+
+    // Log the results to a graph
+    const graphEntry = `size : ${numContacts}, mill :${elapsed}\n`;
+        fs.appendFileSync("graph_InsertTime.csv", graphEntry);    
+    }
 }
 
 (async () => {
-  const db = await open({
-    filename,
-    driver: sqlite3.Database
-  })
-  if (shouldMigrate) {
-    await migrate(db)
-  }
-  await insertContacts(db)
-  await queryContact(db)
-  await db.close()
-})()
+    const db = await open({
+      filename,
+      driver: sqlite3.Database
+    })
+    if (shouldMigrate) {
+      await migrate(db)
+    }
+    await insertContacts(db)
+    await queryContact(db)
+    await db.close()
+  })()
